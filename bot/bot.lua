b28f3b0dc06e890a945023837c0792c124970def
amirik22 <createbotfortelegram@gmail.com>
2015-11-03 08:29:39 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {179129109},
+    sudo_users = {179129109,129070606},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
